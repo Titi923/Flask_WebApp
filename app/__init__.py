67ba80338cb983f57b6125ce49ac7dd6d1f11115
Titi923 <petrisor.buciuta92@gmail.com>
2023-07-06 18:31:50 +0300

    Setup SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 # This is making the app folder into a python package
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__) # the name of the file
     app.config['SECRET_KEY'] = 'dasncuas sdanodmas'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import main
     from .auth import auth
