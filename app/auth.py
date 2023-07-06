15d0fab9f2224f1728d9d1f32e27e6f0432dcda1
Titi923 <petrisor.buciuta92@gmail.com>
2023-07-06 16:29:35 +0300

    Handling the POST requests

--- app/auth.py
+++ app/auth.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html", isLoggedIn=False)
+
+@auth.route('/logout')
+def logout():
+    return "<h1>Logged out</h1>"
 
 @auth.route('/sign-up')
 def signup():
-    return render_template("signUp.html")
+    if request.form == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+
+    return render_template("signUp.html", methods=['GET', 'POST'])
