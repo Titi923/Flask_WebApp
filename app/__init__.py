ec87973b7e2302d96123fd1b68530350390b0af1
Titi923 <petrisor.buciuta92@gmail.com>
2023-07-06 11:32:17 +0300

    create route files

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,10 @@ def create_app():
     app = Flask(__name__) # the name of the file
     app.config['SECRET_KEY'] = 'dasncuas sdanodmas'
     
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, urlprefix="/")
+    app.register_blueprint(auth, urlprefix="/")
+    
     return app
