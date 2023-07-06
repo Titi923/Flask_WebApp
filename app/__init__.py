0d7df4089fdf150bc45171b2ae17cfff201fb544
Titi923 <petrisor.buciuta92@gmail.com>
2023-07-06 11:21:26 +0300

    Created flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,9 @@
 # This is making the app folder into a python package
 
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__) # the name of the file
+    app.config['SECRET_KEY'] = 'dasncuas sdanodmas'
+    
+    return app
