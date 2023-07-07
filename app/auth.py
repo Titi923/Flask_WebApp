735937f15e784e946e66961c64bd46a9cb759d25
Titi923 <petrisor.buciuta92@gmail.com>
2023-07-07 10:14:42 +0300

    Creating New User Accounts

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -20,8 +23,8 @@ def signup():
 
         if len(email) < 4:
             flash('Email already exists.', category='error')
-        # elif len(email) < 4:
-        #     flash('Email must be greater than 3 characters.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
@@ -29,6 +32,10 @@ def signup():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
-            return render_template("signUp.html", methods=['GET', 'POST'])
+            return redirect(url_for('main.home'))
+            
     return render_template("signUp.html", methods=['GET', 'POST'])
